8123d3046ab6171620c2aa6c1a07322a98178b1a
KhaaZ <khaaz.dev@gmail.com>
2019-06-04 20:55:42 +0100

    Actually export Say and Embed

--- src/modules/Moderation/commands/index.js
+++ src/modules/Moderation/commands/index.js
@@ -4,4 +4,5 @@ export { default as Unban } from './Unban';
 export { default as Mute } from './Mute';
 export { default as Unmute } from './Unmute';
 export { default as Warn } from './Warn';
-
+export { default as Say } from './Say';
+export { default as Embed } from './Embed';
